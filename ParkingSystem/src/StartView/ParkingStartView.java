64aef4d26bb2002f1d6f93d772a96466d9c649ec
김희태 <propose0506@naver.com>
2016-02-11 15:49:39 +0900

    이미지 움직이기 실패

--- ParkingSystem/src/StartView/ParkingStartView.java
+++ ParkingSystem/src/StartView/ParkingStartView.java
@@ -4,10 +4,14 @@ import StartProgram.*;
 import StartView.*;
 import MainFunctionView.*;
 
+import java.io.*;
+
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
 import java.util.*;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.event.*;
 
@@ -43,11 +47,6 @@ public class ParkingStartView extends JFrame {
 	JButton newRegBtn = new JButton("회원가입");
 	JButton notMemBtn = new JButton("비회원");
 	
-	//움직이는 자동차를 위한 컴포넌트들
-	ImageIcon moveCarImgIcon;
-	JLabel moveCarLabel;
-	
-	
 	int memCheck = 0; //관리자와 고객을 구분하기 위한 변수
 	String memName = ""; //회원이면 그 회원의 이름을 System화면에 전달하기 위한 변수
 	
@@ -76,7 +75,30 @@ public class ParkingStartView extends JFrame {
 		
 		makeMoveImage(); //맨 처음 로그인 화면에 보이는 차가 움직이는 그림을 구현한 메소드
 		login(); //메인화면 아래에 로그인과 회원가입이 달린 패널 출력
-		new MakeMoveCar(this);
+		
+		/*MakeMoveCar mc = new MakeMoveCar(ParkingStartView.this);
+		frameContentPane.add(mc);
+		Thread thread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				x = (int)(Math.random() * 1200);
+				y = (int)(Math.random() * 700);
+				
+				mc.repaint();
+				//mc.setLocation(x, y);
+				mc.setBounds(x, y, 140, 100);
+				//repaint();
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+		thread.start();*/
+
 		setVisible(true); //ParkingStartView가 상속받은 JFrame의 ContentPane On!
 		
 		FileSystem.loadMemberInfo(); //파일에 저장된 회원정보 리스트를 읽어온다.
@@ -170,48 +192,33 @@ public class ParkingStartView extends JFrame {
 		loginPanel.add(logoImgLabel); //이미지 Label 패널에 추가
 	} //login() End
 	
-	class MakeMoveCar {
+	/*class MakeMoveCar extends JLabel {
 		ParkingStartView frame;
 		int x;
 		int y;
-		//Graphics gImg = carImg.getGraphics();
-		ImageIcon carImgIcon = new ImageIcon("로고1.png");
-		Image carImg = carImgIcon.getImage();
+		
+		ImageIcon moveCarImgIcon;
+		Image moveCarImg;
+
 		public MakeMoveCar(ParkingStartView frame)
 		{
 			this.frame = frame;
 			
-			repaint();
+			moveCarImgIcon = new ImageIcon("로고1.png");	
+			moveCarImg = moveCarImgIcon.getImage();
+			moveCarImg = moveCarImg.getScaledInstance(140,100, java.awt.Image.SCALE_SMOOTH);
 		}
 		
-		public void paint(Graphics g)
+		public void paintComponent(Graphics g)
 		{
-			if (carImg == null)
+			//super.paintComponent(g);
+			if (moveCarImg == null)
 			{
 				return;
 			}
-			g.drawImage(carImg, 500,500, frame);
-		}
-		
-		/*Thread thread = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				// TODO Auto-generated method stub
-				x = (int)(Math.random() * 1200);
-				y = (int)(Math.random() * 700);
-				
-				gImg.drawLine(x, y, x, y);
-				repaint();
-				
-				try {
-					Thread.sleep(1000);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		});*/
-	}
+			g.drawImage(moveCarImg, 0,0, this);
+		}		
+	}*/
 	
 	class LoginAction implements ActionListener //'로그인'버튼 클릭 시
 	{
